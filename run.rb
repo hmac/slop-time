a957b1bbd6d8d3f67b805ca9235f17d546f9de9a
Harry Maclean <harry@hmac.dev>
2024-03-16 10:17:32 +0000

    remove bundle exec

--- run.rb
+++ run.rb
@@ -13,3 +13,10 @@ puts "Found #{links.length} links:"
 links.each do |link|
   puts link
 end
+
+# TODO:
+# 1. read existing links from a file
+# 2. filter out the ones that already exist
+# 3. write the new ones to the file
+# 4. create an issue for each new link
+# 5. tag me?
