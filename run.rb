59d7bcd9a2c3e07d3e7888df1c16de0103cb3001
Harry Maclean <harry@hmac.dev>
2024-03-16 12:30:49 +0000

    Create issues for new links

--- run.rb
+++ run.rb
@@ -9,14 +9,30 @@ page = Net::HTTP.get(URI(events_url))
 puts "Finding event links"
 links = page.scan(href_pattern)
 
-puts "Found #{links.length} links:"
-links.each do |link|
-  puts link
+puts "Found #{links.length} links."
+
+puts "Fetching existing links."
+existing_links = File.readlines("links.txt")
+puts "Found #{existing_links.length} existing links."
+
+# Filter out the ones that already exist
+new_links = links - existing_links
+
+if new_links.empty?
+  puts "There are no new links, exiting."
+  exit(0)
 end
 
-# TODO:
-# 1. read existing links from a file
-# 2. filter out the ones that already exist
-# 3. write the new ones to the file
-# 4. create an issue for each new link
-# 5. tag me?
+puts "There are #{new_links.length} new links:"
+links.each { |link| puts link }
+
+puts "Writing new links to links.txt"
+File.open("links.txt", "a") do |file|
+  new_links.each { |l| file.puts(l) }
+end
+
+puts "Creating issues for new links"
+new_links.each do |link|
+  issue_body = "#{link}\n\n@hmac"
+  `gh issue create --title "#{link}" --body "#{issue_body}"`
+end
